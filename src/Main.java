d6ec9801b77b0c5708d614498fa787bdd7183f71
mohammad_afzalzadeh <mohammad.afzalzadeh.3@gmail.com>
2022-01-01 20:26:31 +0330

    Make Main Page For Messenger

--- src/Main.java
+++ src/Main.java
@@ -39,10 +39,12 @@ public class Main extends Application {
         pass.getChildren().addAll(passLabel, passField);
         root.getChildren().add(pass);
         Button logIn = new Button("ورود");
-        logIn.setOnMouseClicked(event -> {
+        logIn.setOnAction(event -> {
             Person prs = new Person(userNameTextField.getText() , passField.getText());
-            if (sql.LogInPerson(prs))
+            if (sql.LogInPerson(prs)){
                 showAlert("ورود به سامانه" , "" , "می توانید وارد شوید" , false);
+                setMainPage(stage);
+            }
             else
                 showAlert("ورود به سامانه" , "" , "نمی توانید وارد شوید" , true);
 
@@ -53,6 +55,7 @@ public class Main extends Application {
         signIn.setOnMouseClicked(event -> { setSignInPage(stage); });
         Scene firstPage = new Scene(root);
         stage.setScene(firstPage);
+        stage.setTitle("صفحه ورورد");
     }
 
     private void setSignInPage(Stage stage) {
@@ -98,6 +101,57 @@ public class Main extends Application {
         firstPage.setOnMouseClicked(event -> { setLogInPage(stage); });
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setTitle("صفحه ثبت نام");
+    }
+
+    private void setMainPage(Stage stage){
+        HBox root = new HBox(20);
+        root.setAlignment(Pos.CENTER);
+        VBox right = new VBox(20);
+        right.setAlignment(Pos.CENTER);
+        /*Button showPrs = new Button("نمایش اطلاعات کاربر");
+        showPrs.setOnAction(event -> {
+
+        });*/
+        ScrollPane Groups = new ScrollPane();
+        Button addGrp = new Button("افزودن گروه");
+        addGrp.setOnAction(event -> {
+
+        });
+        right.getChildren().addAll(/*showPrs ,*/ Groups , addGrp);
+
+
+        VBox left = new VBox(20);
+        left.setAlignment(Pos.CENTER);
+        ScrollPane messages = new ScrollPane();
+
+        Label txt = new Label();
+        txt.setAlignment(Pos.CENTER);
+        txt.setText( " سلام حسین چطوری ؟");
+        for (int i = 0; i < 100; i++) {
+            txt.setText(txt.getText() + "\n\n سلام حسین چطوری ؟");
+        }
+
+        messages.setContent(txt);
+
+
+        HBox msg = new HBox(1);
+        msg.setAlignment(Pos.CENTER);
+        TextField txtMsg = new TextField();
+        Button send = new Button("s");
+        send.setOnAction(event -> {
+
+        });
+        msg.getChildren().addAll(send , txtMsg);
+        left.getChildren().addAll(messages , msg );
+        root.getChildren().add(left);
+
+        root.getChildren().add(right);
+
+        Scene mainPage = new Scene(root);
+        stage.setScene(mainPage);
+        stage.setTitle("صفحه اصلی");
+
     }
 
     private static void showAlert(String title , String headerText ,String mainText , boolean isError) {
