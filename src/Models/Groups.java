69109d55e71c83fc1a7812faa9743989accf7ba5
mohammad_afzalzadeh <mohammad.afzalzadeh.3@gmail.com>
2022-01-08 11:55:55 +0330

    Add Key To Code Msg in Grp

--- src/Models/Groups.java
+++ src/Models/Groups.java
@@ -1,17 +1,25 @@
 package Models;
 
+import java.util.Random;
+
 public class Groups {
     private long gId;
     private String name;
     private String detail;
+    private String key;
 
     public Groups(long groupId , String name, String detail) {
-        this.name = name;
+       this(groupId , name , detail , getRandomString(10));
+    }
+
+    public Groups(long groupId, String name, String detail, String key) {
         gId = groupId;
+        this.name = name;
         if (detail == "")
             this.detail = null;
         else
             this.detail = detail;
+        this.key = key;
     }
 
     public String getName() {
@@ -26,5 +34,20 @@ public class Groups {
 
     public long getGrpId() {
         return gId;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public static String getRandomString(int len){
+        String str = "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                "1234567890" +"!@#$%^&*_-+=";
+        String res = "";
+        Random rand = new Random();
+        for (int i = 0; i < len; i++)
+            res += str.charAt(rand.nextInt(74));
+
+        return res;
     }
 }
