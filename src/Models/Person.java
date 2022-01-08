0f204ab9e314b42c71ecbcfff3cb6b6cdf72ee73
mohammad_afzalzadeh <mohammad.afzalzadeh.3@gmail.com>
2022-01-08 13:53:12 +0330

    Save Hash Pasword in database

--- src/Models/Person.java
+++ src/Models/Person.java
@@ -1,5 +1,8 @@
 package Models;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Person {
     private int prsId;
     private String fullName;
@@ -14,7 +17,7 @@ public class Person {
     public Person(String fullName, String userName, String pass, String email) {
         this.fullName = fullName;
         this.userName = userName;
-        Pass = pass;
+        Pass = hash(pass);
         Email = email;
         this.prsId = -1;
     }
@@ -41,5 +44,29 @@ public class Person {
 
     public int getPrsId() {
         return prsId;
+    }
+
+    public static String hash(String input){
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA1");
+            md.update(input.getBytes());
+            byte[] output = md.digest();
+            return bytesToHex(output);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private static String bytesToHex(byte[] b) {
+        char hexDigit[] = {'0', '1', '2', '3', '4', '5', '6', '7',
+                '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        StringBuffer buf = new StringBuffer();
+        for (int j=0; j<b.length; j++) {
+            buf.append(hexDigit[(b[j] >> 4) & 0x0f]);
+            buf.append(hexDigit[b[j] & 0x0f]);
+        }
+        return buf.toString();
     }
 }
